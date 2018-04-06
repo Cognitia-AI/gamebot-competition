6515d80cb3b582fe8f3c28b865ea146e3c2c2b07
Abeer Noor <abeer1342@gmail.com>
2018-04-06 05:05:20 +0500

    Added Bot class in Java API

--- JavaAPI/Controller.java
+++ JavaAPI/Controller.java
@@ -62,23 +62,6 @@ public class Controller
         }
         return null;
     }
-    Command fight(GameState gameState){
-        //the function you have to implement
-        Command myCommand=new Command();
-        if (player.equals("1")){
-             myCommand.playerButtons.up=true; //Example of changing the values (Jumping)   
-            }
-        else if (player.equals("2")){
-            myCommand.player2Buttons.up=true;
-        }
-        /*
-                    YOUR CODE GOES HERE
-        You need to change the values of the playerButtons if your player is player1 and if your player
-        is player2 then change player2Buttons respectively.
-        
-        */
-        return myCommand;
-    }
     void send(Command myCommand){
         //This function will send your updated command to Bizhawk so that game reacts according to your command.
         JSONObject json=myCommand.ObjectToJSON();
@@ -95,15 +78,16 @@ public class Controller
     }
     public static void main(String []args)
     {
-        Controller aiBot=new Controller(args[0]); //Initialize
-        aiBot.connect(); //connect to Game
+        Controller controller=new Controller(args[0]); //Initialize
+        controller.connect(); //connect to Game
         GameState currentGameState=null;
+        Bot bot = new Bot();
         int round_count=0;
         boolean roundSupport=false;
         while (currentGameState==null || currentGameState.isRoundOver==false){
-            currentGameState=aiBot.receive(); //get the current state of the game
-            Command myCommand=aiBot.fight(currentGameState); //get the command
-            aiBot.send(myCommand); //send command to the game.
+            currentGameState=controller.receive(); //get the current state of the game
+            Command myCommand=Bot.fight(currentGameState, args[0]); //get the command
+            controller.send(myCommand); //send command to the game.
         }
     }
 }
